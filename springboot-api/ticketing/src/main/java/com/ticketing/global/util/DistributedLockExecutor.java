82b1c15846f2f3d356746b8ce7d7d9ce696bb755
changyunyeong <sally0109277@naver.com>
2025-12-17 17:15:30 +0900

    feat: add util

--- springboot-api/ticketing/src/main/java/com/ticketing/global/util/DistributedLockExecutor.java
+++ springboot-api/ticketing/src/main/java/com/ticketing/global/util/DistributedLockExecutor.java
@@ -1,5 +1,6 @@
 package com.ticketing.global.util;
 
+import com.ticketing.global.exception.domain.LockAcquisitionException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
@@ -16,23 +17,57 @@ public class DistributedLockExecutor {
 
     private final RedissonClient redissonClient;
 
-    public <T> T executeWithLock(String lockKey, long waitTime, long leaseTime, Supplier<T> supplier) {
+    /**
+     * 분산 락으로 보호된 작업 실행
+     *
+     * @param lockKey 락 키
+     * @param waitTime 대기 시간 (초)
+     * @param leaseTime 락 보유 시간 (초)
+     * @param supplier 실행할 작업
+     * @return 작업 결과
+     */
+    public <T> T executeWithLock(
+            String lockKey,
+            long waitTime,
+            long leaseTime,
+            Supplier<T> supplier) {
+
         RLock lock = redissonClient.getLock(lockKey);
 
         try {
             boolean available = lock.tryLock(waitTime, leaseTime, TimeUnit.SECONDS);
+
             if (!available) {
-                throw new IllegalStateException("Lock을 획득하지 못했습니다: " + lockKey);
+                log.warn("Failed to acquire lock: {}", lockKey);
+                throw new LockAcquisitionException();
             }
 
+            log.debug("Lock acquired: {}", lockKey);
             return supplier.get();
+
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException("Lock 획득 중 인터럽트 발생", e);
         } finally {
             if (lock.isHeldByCurrentThread()) {
                 lock.unlock();
+                log.debug("Lock released: {}", lockKey);
             }
         }
+    }
+
+    /**
+     * void 반환 작업용
+     */
+    public void executeWithLock(
+            String lockKey,
+            long waitTime,
+            long leaseTime,
+            Runnable runnable) {
+
+        executeWithLock(lockKey, waitTime, leaseTime, () -> {
+            runnable.run();
+            return null;
+        });
     }
 }
