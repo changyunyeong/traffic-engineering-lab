d59b165e1ffd41d314850b80c31b2375a8924287
changyunyeong <sally0109277@naver.com>
2025-12-24 23:21:02 +0900

    feat: fix redis cache

--- springboot-api/ticketing/src/main/java/com/ticketing/global/config/RedisConfig.java
+++ springboot-api/ticketing/src/main/java/com/ticketing/global/config/RedisConfig.java
@@ -91,28 +91,6 @@ public class RedisConfig {
     /**
      * 캐시 매니저 설정
      */
-//    @Bean
-//    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.registerModule(new JavaTimeModule());
-//        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-//
-//        GenericJackson2JsonRedisSerializer serializer =
-//                new GenericJackson2JsonRedisSerializer(objectMapper);
-//
-//        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-//                .entryTtl(Duration.ofHours(1))
-//                .serializeKeysWith(
-//                        RedisSerializationContext.SerializationPair
-//                                .fromSerializer(new StringRedisSerializer()))
-//                .serializeValuesWith(
-//                        RedisSerializationContext.SerializationPair
-//                                .fromSerializer(serializer));
-//
-//        return RedisCacheManager.builder(connectionFactory)
-//                .cacheDefaults(config)
-//                .build();
-//    }
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator
